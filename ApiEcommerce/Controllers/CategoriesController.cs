f866c7a9b13ac0f8733bcee54f2ccbdb56a7f03c
Jose Marcial Galván <carrona43@gmail.com>
2026-09-14 08:44:31 +0100

    Post endpoint created

--- ApiEcommerce/Controllers/CategoriesController.cs
+++ ApiEcommerce/Controllers/CategoriesController.cs
@@ -54,5 +54,33 @@ namespace ApiEcommerce.Controllers
         {
             return _mapper.Map<CategoryDto>(category);
         }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public  IActionResult CreateCategory([FromBody] CreateCategoryDto createCategoryDto)
+        {
+            if( createCategoryDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (_categoryRepository.Exists(createCategoryDto.Name))
+            {
+                ModelState.AddModelError("CustomError", "La categoría ya existe");
+                return BadRequest(ModelState);
+            }
+            Category category = _mapper.Map<Category>(createCategoryDto);
+            if (!_categoryRepository.CreateCategory(category))
+            {
+                ModelState.AddModelError("CustomError", $"Algo salió mal {category}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetCategory", new {id = category.Id}, category);
+        }
     }
 }
