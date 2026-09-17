b67da4ea7323bc411499a4d2ba84b713dc585898
Jose Marcial Galván <carrona43@gmail.com>
2026-09-17 14:19:35 +0100

    feat: User repository implemented

--- ApiEcommerce/Repository/UserRepository.cs
+++ ApiEcommerce/Repository/UserRepository.cs
@@ -1,17 +1,24 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.AccessControl;
+using System.Security.Claims;
+using System.Text;
 using ApiEcommerce.Migrations;
 using ApiEcommerce.Model;
 using ApiEcommerce.Model.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiEcommerce.Repository.IRepository;
 
 public class UserRepository : IUserRepository
 {
     public readonly ApplicationDbContext  _db;
-
-    public UserRepository(ApplicationDbContext db)
+    private string? secretKey;
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
     }
 
     public User? GetUSer(int id)
@@ -31,13 +38,82 @@ public class UserRepository : IUserRepository
 
     private string Normalize(string str) => str.ToLower().Trim();
 
-    public Task<UserLogingResponseDto> Login(UserLoginDto userLoginDto)
+    public async Task<UserLogingResponseDto> Login(UserLoginDto userLoginDto)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(userLoginDto.UserName))
+        {
+            return new UserLogingResponseDto()
+            {
+                Token = "",
+                User = null,
+                 Message = "El username es requerido"
+            };
+        }
+        User? user = await _db.Users.FirstOrDefaultAsync(user => user.UserName.ToLower().Trim() == userLoginDto.UserName.ToLower().Trim());
+        if (user == null)
+        {
+            return new UserLogingResponseDto()
+            {
+                Token = "",
+                User = null,
+                 Message = "Usuario no encontrado"
+            };
+        }
+        if (!BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
+        {
+            return new UserLogingResponseDto()
+            {
+                Token = "",
+                User = null,
+                Message = "Credenciales incorrectas"
+            };
+        }
+        JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+        if (string.IsNullOrWhiteSpace(secretKey))
+        {
+            throw new InvalidOperationException("Secret Key is not configurated");
+        }
+        byte[] key = Encoding.UTF8.GetBytes(secretKey);
+        var tockenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("id", user.Id.ToString()),   
+                new Claim("username", user.UserName.ToString()),
+                new Claim(ClaimTypes.Role, user.Role ?? string.Empty),
+            }),
+            Expires =  DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = TokenHandler.CreateToken(tockenDescriptor);
+        return new UserLogingResponseDto()
+        {
+            Token = TokenHandler.WriteToken(token),
+            User = new UserRegisterDto()
+            {
+                UserName = user.UserName,
+                Name = user.Name,
+                Role = user.Role,
+                Password = user.Password
+            },
+            Message = "usuario logeado correctamente"
+        };
     }
 
-    public Task<User> Register(CreateUserDto createUserDto)
+    public async Task<User> Register(CreateUserDto createUserDto)
     {
-        throw new NotImplementedException();
+        string encriptedPassword = BCrypt.Net.BCrypt.HashPassword(createUserDto.Password);
+        User user = new User()
+        {
+            UserName = createUserDto.UserName ?? "No UserName",
+            Name = createUserDto.Name,
+            Role = createUserDto.Role,
+            Password = encriptedPassword
+        };
+
+        _db.Users.Add(user);
+        await _db.SaveChangesAsync();
+        return user;
+
     }
 }
