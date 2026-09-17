6ce122509f50b8a7e351856241e8714e8eb71e23
Jose Marcial Galván <carrona43@gmail.com>
2026-09-17 16:22:33 +0100

    feat: Implement UserController with user registration, login, and retrieval endpoints

---
+++ ApiEcommerce/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+using ApiEcommerce.Model.Dtos;
+using ApiEcommerce.Model;
+using ApiEcommerce.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase{
+    public readonly IMapper _mapper;
+    public readonly IUserRepository _userRepository;
+
+        public UserController(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet(Name = "GetUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public IActionResult GetUsers()
+        {
+            return Ok(_mapper.Map<List<UserDto>>(_userRepository.GetUSers()));  
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUser(int id)
+        {
+            if (id <= 0)
+            {
+                ModelState.AddModelError("CustomError", "El id tiene que ser >= 0");
+                return BadRequest(ModelState);
+            }
+            User? user = _userRepository.GetUSer(id);
+            if (user == null)
+            {
+                return NotFound("User not foud");
+            }
+            return Ok(_mapper.Map<UserDto>(user));
+        }
+
+
+        [HttpPost(Name = "RegisterUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserDto createUserDto)
+        {
+            if (createUserDto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (string.IsNullOrWhiteSpace(createUserDto.UserName))
+            {
+                return BadRequest("Username is required");
+            }
+            if (_userRepository.Exists(createUserDto.UserName))
+            {
+                return BadRequest("the user already exists");
+            }
+            User user = await _userRepository.Register(createUserDto);
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return CreatedAtRoute("GetUser", new {id = user.Id}, user);
+        }
+
+        
+        [HttpPost("Login", Name = "LoginUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> LoginUser([FromBody] UserLoginDto userLogingDto)
+        {
+            if (userLogingDto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            UserLogingResponseDto userLogingResponseDto = await _userRepository.Login(userLogingDto);
+            if (userLogingResponseDto == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(userLogingResponseDto);
+        }
+    }
+    
+
+}
